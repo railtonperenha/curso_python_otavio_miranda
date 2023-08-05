583f2321089d9e3236548c2646ada4a94bee5b29
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-08-05 20:05:04 -0300

    Create aula27.py

---
+++ aulas/aula27.py
@@ -0,0 +1,11 @@
+'''
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs. A função len retorna a qtd de caracteres da str
+'''
+
+variavel = 'Olá Mundo'
+print(variavel[5])
