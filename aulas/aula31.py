68629eb644b0874f1e2b8808e4f260b683bb0cd6
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-08-07 22:50:51 -0300

    Create aula30, aula31

--- aulas/aula31.py
+++ aulas/aula31.py
@@ -5,11 +5,20 @@ is e is not = é ou não é (tipo, valor, identidade)
 id = identidade
 '''
 
-v1 = 'a'
-v2 = 'b'
+condicao = False
+passou_no_if = None
 
-print(id(v1))
-print(id(v2))
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+
+else:
+    print('Passou no if')
 
 # condicao = False
 
