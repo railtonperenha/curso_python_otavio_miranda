dcd17b169e0cf2770449228239789bad1d3a47f4
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-09-10 21:59:25 -0300

    Update

--- aulas/aula36.py
+++ aulas/aula36.py
@@ -1,22 +1,10 @@
-# Operadores de atribuição
-# = += -= *= /= //= **= %=
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 10
 
-contador = 0
+###
 
-while contador <= 100:
-    contador += 1
-
-    if contador == 6:
-        print('Não vou mostrar o 6')
-        continue
-
-    if contador >= 10 and contador <= 27:
-        print('Não vou mostrar o', contador)
-        continue
-
-    print(contador)
-
-    if contador == 40:
-        break
-
-print('Acabou!')
+contador /= 5
+print(contador)
